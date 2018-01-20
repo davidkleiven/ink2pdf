bb8638d8353d5eb09f0600d4390879c5701b54dc
David Kleiven <davidkleiven446@gmail.com>
2018-01-20 12:41:29 +0100

    Travis script

---
+++ tests.sh
@@ -0,0 +1,10 @@
+# Script for running tests
+
+# Regular no font specified
+./ink2pdf exampleSvg.svg
+
+# Specify fontsize
+./ink2pdf exampleSvg.svg 14pt
+
+# Specify fontsize and sans font
+./ink2pdf exampleSvg.svg 10pt sans
